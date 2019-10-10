d0f5663a1165b5b205f622006adf464fa124522e
Sven Gotal <sven.gotal.acc@hotmail.com>
2019-10-10 20:40:13 +0200

    Add application menu

--- MyEditor/FormMain.cs
+++ MyEditor/FormMain.cs
@@ -16,5 +16,15 @@ namespace MyEditor
         {
             InitializeComponent();
         }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
