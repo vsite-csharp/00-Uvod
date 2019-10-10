8bc6e9fe05506064b02207b55f9b760c8be83c4d
ivana474 <54909467+ivana474@users.noreply.github.com>
2019-10-10 20:44:21 +0200

    Implement window closing

--- MyEditor/FormMain.cs
+++ MyEditor/FormMain.cs
@@ -16,5 +16,10 @@ namespace MyEditor
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
