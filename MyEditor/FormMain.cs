ba89903f37e5caaaad6a23d6676941e753530149
Auro Mazzi <auro.mazzi@gmail.com>
2019-10-10 20:40:18 +0200

    Add application menu

--- MyEditor/FormMain.cs
+++ MyEditor/FormMain.cs
@@ -16,5 +16,10 @@ namespace MyEditor
         {
             InitializeComponent();
         }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
