2bffb49f2a763f5875922f9882e25aad231108dd
Ivan Bitunjac <ivbitunj@vsite.hr>
2019-10-10 20:44:01 +0200

    Implemented window closing

--- WindowsFormsApp1/FormMain.cs
+++ WindowsFormsApp1/FormMain.cs
@@ -21,5 +21,10 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
