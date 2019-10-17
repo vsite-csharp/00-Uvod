474747bf5f9871df360dc6e70824d65aa8115ac7
JosipVuckovic <josip.vuckovic@live.com>
2019-10-17 19:49:40 +0200

    removal of icon

--- MyEditor/Program.cs
+++ MyEditor/Program.cs
@@ -16,7 +16,8 @@ namespace MyEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormMain());  
+
         }
     }
 }
