b0c5ca193b81e61a574c26037c3f0c349ddc4219
pastabil <pastabil@vsite.hr>
2020-10-08 21:14:14 +0200

    Implement File - Exit

--- MyWord/MainForm.Designer.cs
+++ MyWord/MainForm.Designer.cs
@@ -93,8 +93,9 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(137, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // editToolStripMenuItem
             // 
