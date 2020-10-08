31866d1d76fcee2cfebc1b6be13c1d5c541eb176
Julijan Sribar <julijan.sribar@gmail.com>
2020-10-08 21:32:51 +0200

    Implement File-Save

--- MyWord/MainForm.cs
+++ MyWord/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,18 @@ namespace MyWord
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
             textBox.Clear();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                var filename = saveFileDialog.FileName;
+                using (StreamWriter sw = new StreamWriter(filename))
+                {
+                    sw.Write(textBox.Text);
+                }
+            }
         }
     }
 }
