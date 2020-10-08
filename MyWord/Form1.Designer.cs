98bc4f62a88853f91cb4d618d2bcc4058f45a1dd
Maja Smolković <maja.smolkovic@gmail.com>
2020-10-08 21:14:13 +0200

    implement File Exit

--- MyWord/Form1.Designer.cs
+++ MyWord/Form1.Designer.cs
@@ -105,6 +105,7 @@
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // editToolStripMenuItem
             // 
