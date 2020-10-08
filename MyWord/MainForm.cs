5b76b5bc9bbf50b520552bb6a997bb1af78b50e7
Klara Blašković <klblasko@vsite.hr>
2020-10-08 21:05:35 +0200

    Add main menu

--- MyWord/MainForm.cs
+++ MyWord/MainForm.cs
@@ -16,5 +16,10 @@ namespace MyWord
         {
             InitializeComponent();
         }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
