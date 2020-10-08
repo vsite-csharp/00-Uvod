d2e10fbbf09afe8a4078913c29d525fc2fd3f6a0
Tomislav Peša <72340847+Topesa@users.noreply.github.com>
2020-10-08 22:40:32 +0200

    add "MyWord" name for app ( final push)

--- MyWord/MainForm.Designer.cs
+++ MyWord/MainForm.Designer.cs
@@ -142,7 +142,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "MyWord";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
