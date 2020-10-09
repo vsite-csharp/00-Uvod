587dd4e7355a3b8ae0466715dcab8dfa2e397ddb
Kristian Petretić <krpetret@vsite.hr>
2020-10-09 10:54:26 +0200

    Add main menu

--- MyWord/MainForm.cs
+++ MyWord/MainForm.cs
@@ -16,5 +16,15 @@ namespace MyWord
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filfeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
