e85cf96b6114a888596b5fdc63d2f68fb5cb4326
Kristian Petretić <krpetret@vsite.hr>
2020-10-15 19:36:28 +0200

    close

--- MyWord/MainForm.cs
+++ MyWord/MainForm.cs
@@ -66,5 +66,12 @@ namespace MyWord
                 }
             }
         }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to quit?", "My Word", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                e.Cancel = true;
+                base.OnClosing(e);
+        }
+
     }
 }
