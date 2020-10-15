4241e01d5d516dcd8698f9754476e6533563de91
Ante Žunić <anzunic@vsite.hr>
2020-10-15 19:31:19 +0200

    Add message box for quitting the application

--- MyWord/MainForm.cs
+++ MyWord/MainForm.cs
@@ -53,5 +53,12 @@ namespace MyWord
 
             }
         }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to quit?", "My Word", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                e.Cancel = true;
+            base.OnClosing(e);
+        }
     }
 }
